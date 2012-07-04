3fb04d8d00b977d1245ca55c1d5f72dfd721e8f8
fernando <fernando@debian.volum>
2012-07-04 11:36:38 -0300

    Tentando criar estrutura básica (ainda com erros)

--- application/config/config.php
+++ application/config/config.php
@@ -358,5 +358,10 @@ $config['rewrite_short_tags'] = FALSE;
 $config['proxy_ips'] = '';
 
 
+$config['modules_locations'] = array(
+     APPPATH.'modules/' => '../modules/',
+     APPPATH.'modules/' => '../cms/modules/',
+);
+
 /* End of file config.php */
 /* Location: ./application/config/config.php */
---
+++ application/modules/cadeiras/controllers/cadeiras.php
@@ -0,0 +1,18 @@
+<?php
+class Cadeiras extends MX_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        echo 'Acessou CMS: Cadeiras';
+    }
+
+}
+
+/* End of file cadeiras.php */
+/* Location: ./application/modules/cms/cadeiras/controllers/cadeiras.php */
